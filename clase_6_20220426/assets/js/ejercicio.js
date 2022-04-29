ad58c786a8b089826d8a9f48c77e40c92b7e2fe2
jcapellanvasquez <53913902+jcapellanvasquez@users.noreply.github.com>
2022-04-28 21:51:48 -0400

    clase 7

--- clase_6_20220426/assets/js/ejercicio.js
+++ clase_6_20220426/assets/js/ejercicio.js
@@ -1,21 +1,32 @@
-// console.log("Hola")
 
-// function simple
-function saludo() {
-    console.log("Hola, desde una funcion")
-}
+// accion para obtener el formulario
+const miFormulario = document.querySelector("#my-form")
 
-
-// function con parametros
-function saludos(evento) {
-    // evitar que se refresque la pagina
+function eventoSubmit(evento) {
+    // Quitar comportamiento por defecto del evento
     evento.preventDefault()
 
-    console.log("Hola, desde una funcion")
+    // Desde aqui va mi logica para el evento...
+    
+    // Obtener los inputs del formulario
+    const inputs = evento.target.elements;
+
+    // Obtener los valos de los inputs
+    const contenido = inputs["contenido"].value;
+    const altura =  inputs["altura"].value;
+    const ancho = inputs["ancho"].value;
+
+    // Cambiar texto de la caja
+    const elementoCaja = document.querySelector("#caja")
+    elementoCaja.textContent = contenido;
+
+    // Cambiar ancho de la caja
+    elementoCaja.style.width = ancho + 'px'
+
+    // Cambiar altura de la caja
+    elementoCaja.style.height = altura + 'px'
 }
 
-const miFormulario = document.querySelector("#my-form")
-console.log(miFormulario)
 
-
-miFormulario.addEventListener('submit', saludos)
+miFormulario.addEventListener("submit", eventoSubmit)
+// eventoSubmit(evento)
